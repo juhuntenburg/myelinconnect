94965b77ddcaa53bb97fa33ef60c16dd54c1b34a
juhuntenburg <ju.huntenburg@gmail.com>
2015-10-13 10:58:22 +0200

    parallelizing complex_to_simple

--- complex_to_simple_mesh.py
+++ complex_to_simple_mesh.py
@@ -4,6 +4,7 @@ import scipy.spatial as spatial
 import sys
 import time
 import pandas as pd
+from joblib import Memory, Parallel, delayed
 # from https://pypi.python.org/pypi/bintrees/2.0.2
 from bintrees import FastAVLTree
 # from https://github.com/juhuntenburg/brainsurfacescripts
@@ -11,28 +12,27 @@ from vtk_rw import read_vtk, write_vtk
 from graphs import graph_from_mesh
 
 '''
-This script maps vertices from a simplified mesh to the closest corresponding 
-vertices of the original mesh using a KDTree. These vertices are 
+Maps vertices from a simplified mesh to the closest corresponding
+vertices of the original mesh using a KDTree. These vertices are
 then used as seeds for a Voronoi tesselation of the complex mesh which is
-implemented as a competitive fast marching in a balanced binary (AVL)tree. 
-Thus a mapping is created which associates each vertex of the complex mesh 
+implemented as a competitive fast marching in a balanced binary (AVL)tree.
+A mapping is created which associates each vertex of the complex mesh
 with the closest vertex of the simple mesh.
+-----------------------------------------------------------------------------
+Binary search trees: https://en.wikipedia.org/wiki/Binary_search_tree
+Balanced binary trees: https://en.wikipedia.org/wiki/AVL_tree,
+Using them as heaps: http://samueldotj.com/blog/?s=binary+tree
+Implementation used: https://pypi.python.org/pypi/bintrees/2.0.2 (cython version)
 '''
 
-# set inputs 
-subjects = pd.read_csv('/scr/ilz3/myelinconnect/subjects.csv')
-subjects=list(subjects['DB'])
-subjects.remove('KSMT')
 
-complex_file = '/scr/ilz3/myelinconnect/struct/surf_%s/orig/mid_surface/%s_%s_mid.vtk'
-simple_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/lowres_%s_d_def.vtk'# version d
+# function returning generator for sub,hemi tuples fro joblib
+def tupler(subjects, hemis, log_file):
+    for s in subjects:
+        for h in hemis:
+            yield (s, h, log_file)
 
-label_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/%s_%s_highres2lowres_d_labels.txt'
-surf_label_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/%s_%s_highres2lowres_d_labels.vtk'
-
-log_file = '/home/raid3/huntenburg/workspace/myelinconnect/complex_to_simple_log.txt'
-
-# function to find unlabelled neighbours of a node in the graph 
+# function to find unlabelled neighbours of a node in the graph
 # and add them to the tree correctly
 def add_neighbours(node, graph, labels, tree):
     # find direct neighbours of the node
@@ -44,114 +44,114 @@ def add_neighbours(node, graph, labels, tree):
         tree.insert(graph[node][u]['length'],(node, u))
     return tree
 
+# function to write time and message to log file
+def log(log_file, message, logtime=True):
+    with open(log_file, 'a') as f:
+        if timing:
+            f.write(time.ctime()+'\n')
+        f.write(message+'\n')
+
+# main function (might be good to split eventually)
+@memory.cache
+def create_mapping((sub, hemi, log_file)):
+
+    # log different steps in logfile
+    log(log_file, 'Processing %s %s'%(sub, hemi))
+
+    # not optimal to have this hardcoded inside the function
+    # figure out how to pipe with parallel and move outside
+    complex_file = '/scr/ilz3/myelinconnect/struct/surf_%s/orig/mid_surface/%s_%s_mid.vtk'
+    simple_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/lowres_%s_d_def.vtk'# version d
+    label_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/%s_%s_highres2lowres_d_labels.txt'
+    surf_label_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/%s_%s_highres2lowres_d_labels.vtk'
+
+    # load the meshes
+    log(log_file, '...loading data', logtime=False)
+    complex_v,complex_f, complex_d = read_vtk(complex_file%(hemi, sub, hemi))
+    simple_v, simple_f, simple_d = read_vtk(simple_file%(sub, hemi))
+
+    # find those points on the individuals complex mesh that correspond best
+    # to the simplified group mesh in subject space
+    log(log_file, '...running kdtree')
+    inaccuracy, mapping = spatial.KDTree(complex_v).query(simple_v)
+    # find coordinates of those points in the highres mesh
+    voronoi_seed_idx = mapping
+    voronoi_seed_coord = complex_v[mapping]
+
+    # double check differences
+    log(log_file, '...checking kdtree')
+    dist = np.linalg.norm((voronoi_seed_coord - simple_v), axis=1)
+    if any(dist != inaccuracy):
+        sys.exit("simple to complex mesh mapping not correct")
+
+    # convert highres mesh into graph containing edge length
+    log(log_file, '...creating graph')
+    complex_graph = graph_from_mesh(complex_v, complex_f, edge_length=True)
+
+    # make a labelling container to be filled with the search tree
+    # first column are the vertex indices of the complex mesh
+    # second column are the labels from the simple mesh
+    # (-1 for all but the corresponding points for now)
+    log(log_file, '...initiating labels')
+    labelling = np.zeros((complex_v.shape[0],2), dtype='int64')-1
+    labelling[:,0] = range(complex_v.shape[0])
+
+    for i in range(voronoi_seed_idx.shape[0]):
+        labelling[voronoi_seed_idx[i]][1] = i
+
+    # initiate AVLTree for binary search
+    log(log_file, '...building search tree')
+    tree = FastAVLTree()
+    # organisation of the tree will be
+    # key: edge length
+    # value: tuple of vertices (source, target)
+
+    # find all neighbours of the voronoi seeds
+    for v in voronoi_seed_idx:
+        add_neighbours(v, complex_graph, labelling, tree)
+
+    # Competetive fast marching starting from voronoi seeds
+    log(log_file, '...marching')
+    while any(labelling[:,1]==-1):
+        while tree.count > 0:
+            # pop the item with minimum edge length
+            min_item = tree.pop_min()
+            source = min_item[1][0]
+            target = min_item[1][1]
+
+            #if target no label yet (but source does!), assign label of source
+            if labelling[target][1] == -1:
+                if labelling[source][1] == -1:
+                    sys.exit('Source has no label, something went wrong!')
+                else:
+                    # assign label of source to target
+                    labelling[target][1] = labelling[source][1]
+
+            # add neighbours of target to tree
+            add_neighbours(target, complex_graph, labelling, tree)
+
+    # write out labelling file and surface with labels
+    log(log_file, '...saving data')
+    np.savetxt(label_file%(sub, sub, hemi), labelling)
+    write_vtk(surf_label_file%(sub, sub, hemi), complex_v, complex_f,
+                data=labelling[:,1, np.newaxis])
+
+    log(log_file, 'Finished %s %s'(sub, hemi))
+
+    return log_file
 
 
-for sub in subjects:
-    
-    for hemi in ['rh', 'lh']:
-        
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('Processing '+sub+' '+hemi+'\n')
-            f.write('...loading data\n')
+if __name__ == "__main__":
 
-        # load the meshes
-        complex_v,complex_f, complex_d = read_vtk(complex_file%(hemi, sub, hemi))
-        simple_v, simple_f, simple_d = read_vtk(simple_file%(sub, hemi))
-        
-        # find those points on the individuals complex mesh that correspond best
-        # to the simplified group mesh in subject space
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('...running kdtree\n')
-            
-        inaccuracy, mapping = spatial.KDTree(complex_v).query(simple_v)
-        
-        # find coordinates of those points in the highres mesh
-        voronoi_seed_idx = mapping
-        voronoi_seed_coord = complex_v[mapping]
-        
-        # double check differences
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('...checking kdtree\n')
-            
-        dist = np.linalg.norm((voronoi_seed_coord - simple_v), axis=1)
-        if any(dist != inaccuracy):
-            sys.exit("simple to complex mesh mapping not correct")
+    subjects = pd.read_csv('/scr/ilz3/myelinconnect/subjects.csv')
+    subjects=list(subjects['DB'])
+    subjects.remove('KSMT')
 
-        # convert highres mesh into graph containing edge length
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('...making graph\n')
-            
-        complex_graph = graph_from_mesh(complex_v, complex_f, edge_length=True)
+    hemis = ['lh', 'rh']
 
-        # make a labelling container to be filled with the search tree
-        # first column are the vertex indices of the complex mesh
-        # second column are the labels from the simple mesh 
-        # (-1 for all but the corresponding points for now)
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('...making labelling\n')
-            
-        labelling = np.zeros((complex_v.shape[0],2), dtype='int64')-1
-        labelling[:,0] = range(complex_v.shape[0])
-        
-        for i in range(voronoi_seed_idx.shape[0]):
-            labelling[voronoi_seed_idx[i]][1] = i
-        
-        # initiate AVLTree for binary search
-        # binary search trees: https://en.wikipedia.org/wiki/Binary_search_tree
-        # balanced binary trees: https://en.wikipedia.org/wiki/AVL_tree, 
-        # using them as heaps: http://samueldotj.com/blog/?s=binary+tree
-        # implementation used: https://pypi.python.org/pypi/bintrees/2.0.2 (cython implementation)
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('...building avltree\n')
-            
-        tree = FastAVLTree()
-        # organisation of the tree will be
-        # key: edge length
-        # value: tuple of vertices (source, target)
+    log_files = '/scr/ilz3/working_dir/complex_to_simple/log.txt'
+    cachedir = '/scr/ilz3/working_dir/complex_to_simple/'
+    memory = Memory(cachedir=cachedir, mmap_mode='r')
 
-
-        # find all neighbours of the voronoi seeds
-        for v in voronoi_seed_idx:
-            add_neighbours(v, complex_graph, labelling, tree)
-        
-        # Competetive fast marching starting from voronoi seeds
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('...marching\n')
-        
-        while any(labelling[:,1]==-1):
-            while tree.count > 0:
-                # pop the item with minimum edge length
-                min_item = tree.pop_min()
-                source = min_item[1][0]
-                target = min_item[1][1]
-        
-                #if target no label yet (but source does!), assign label of source
-                if labelling[target][1] == -1:
-                    if labelling[source][1] == -1:
-                        sys.exit('Source has no label, something went wrong!')
-                    else:
-                        # assign label of source to target
-                        labelling[target][1] = labelling[source][1]
-        
-                # add neighbours of target to tree
-                add_neighbours(target, complex_graph, labelling, tree)
-  
-        # write out labelling file and surface with labels
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('...saving data\n')
-            
-        np.savetxt(label_file%(sub, sub, hemi), labelling)
-        write_vtk(surf_label_file%(sub, sub, hemi), complex_v, complex_f, data=labelling[:,1, np.newaxis])
-        
-        with open(log_file, 'a') as f:
-            f.write(time.ctime()+'\n')
-            f.write('Finished '+sub+' '+hemi+'\n\n')
-        
+    Parallel(n_jobs=8)(delayed(create_mapping)(i)
+                        for i in tupler(subjects, hemis, log_file))
--- sample_to_simple.py
+++ sample_to_simple.py
@@ -5,8 +5,8 @@ from vtk_rw import read_vtk, write_vtk
 from joblib import Memory, Parallel, delayed
 
 '''
-This script takes a highres to lowres labelling, finds all vertices of the 
-highres mesh that are assigned to a given lowres vertex, samples the data 
+Takes a highres to lowres mesh labelling, finds all vertices of the 
+highres mesh that are assigned to a given lowres vertex, samples the data
 from these vertices and assigns their mean to the lowres vertex
 '''
 
@@ -24,42 +24,42 @@ def sample_to_simple_surf(sub, hemi):
     # new_lowres_file = avg
     data_file = '/scr/ilz3/myelinconnect/final_data_on_surfaces/t1/%s_%s_profiles_lowres.npy'%(sub, hemi)
     # data file = avg
-    
+
     label_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/labelling_%s.txt'%(sub, hemi)
     old_lowres_file = '/scr/ilz3/myelinconnect/groupavg/indv_space/%s/lowres_%s_d_def.vtk'%(sub, hemi)
-    
+
     # load data
     labels = np.loadtxt(label_file)
     highres_v, highres_f, highres_data = read_vtk(highres_file)
     lowres_v, lowres_f, lowres_d = read_vtk(old_lowres_file)
-    
+
     # create new empty lowres data array
     lowres_data = np.empty((int(labels[:,1].max()+1), highres_data.shape[1]))
-    
+
     # find all vertices on highres and mean
     for l in range(int(labels[:,1 ].max())):
         patch = np.where(labels[:,1]==l)[0]
         patch_data = highres_data[patch]
         patch_mean = np.mean(patch_data, axis=0)
         lowres_data[l] = patch_mean
-    
+
     # save lowres vtk and txt
     write_vtk(new_lowres_file, lowres_v, lowres_f, data=lowres_data)
     np.save(data_file, lowres_data)
-    
+
     return
-        
-        
+
+
 if __name__ == "__main__":
-    
+
     subjects = pd.read_csv('/scr/ilz3/myelinconnect/subjects.csv')
     subjects=list(subjects['DB'])
     subjects.remove('KSMT')
-    
+
     hemis = ['lh', 'rh']
-    
+
     cachedir = '/scr/ilz3/working_dir/sample_to_simple/'
     memory = Memory(cachedir=cachedir, mmap_mode='r')
-    
+
     Parallel(n_jobs=4)(delayed(sample_to_simple)(i)
                            for i in tupler(subjects, hemis))
