da915725f808f142b778601fd7079c6330dd29e6
Julia Huntenburg <ju.huntenburg@gmail.com>
2015-11-04 12:54:47 +0100

    update clustering to hdf5

---
+++ avg_correlations_sub.py
@@ -0,0 +1,101 @@
+from __future__ import division
+import numpy as np
+from vtk_rw import read_vtk, write_vtk
+import pandas as pd
+import sys
+import h5py
+
+
+subjects = pd.read_csv('/scr/ilz3/myelinconnect/subjects.csv')
+subjects=list(subjects['DB'])
+subjects.remove('KSMT')
+
+#subjects = sys.argv[1]
+#subjects = [subjects]
+
+hemis = ['rh']
+sessions = ['1_1', '1_2', '2_1', '2_2']
+
+rest_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/rest/%s_%s_rest%s.npy'
+corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_%s_rest%s_corr.npy'
+avg_corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_%s_avg_corr.npy'
+sub_avg_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_allsub_avg_corr_thr.npy'
+#sub_avg_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_allsub_avg_corr.npy'
+
+for hemi in hemis:
+    
+    # figure out size of the resulting matrix (N_verticesxN_vertices)
+    get_size = np.load('/scr/ilz3/myelinconnect/all_data_on_simple_surf/rest/%s_%s_rest%s.npy'%(subjects[0], hemi, sessions[0])).shape[0]
+    
+    # create empty matrix for cross subject average
+    sub_avg = np.zeros((get_size, get_size))
+    
+    sub_count = 0
+    for sub in subjects:
+        
+        # find out if correlation matrix has been produced before and load
+        try:
+            avg_corr = np.load(avg_corr_file%(sub, hemi))
+            print 'loaded avg corr '+sub
+            
+        # if not, create avg correlation matrix over all 4 runs
+        except IOError:
+            
+            print 'calculating avg corr '+sub
+            # make empty matrix
+            avg_corr_z = np.zeros((get_size, get_size))
+            
+            sess_count = 0
+            for sess in sessions:
+            
+                # find out if intermediate corr matrix has been saved
+                # (more or less deprecated as it takes too much disk space)
+                try:
+                    corr = np.load(corr_file%(sub, hemi, sess))
+                
+                # otherwise load resting state time series of one run and 
+                # create correlation matrix
+                except IOError:
+                    
+                    rest = np.load(rest_file%(sub, hemi, sess))
+                    corr = np.nan_to_num(np.corrcoef(rest))
+                    del rest
+                    #np.save(corr_file%(sub, hemi, sess), corr)
+                
+                # fisher r-to-z transform correlations
+                corr_z = np.arctanh(corr)
+                del corr
+                # add to cross-run average matrix
+                avg_corr_z += corr_z
+                del corr_z
+                
+                # increase count for later division
+                sess_count += 1
+                print 'Finished '+sess
+            
+            # after all sessions have been processed, divide average by count
+            avg_corr_z = avg_corr_z / sess_count
+            # tranfroms back to r values
+            avg_corr = np.tanh(avg_corr_z)
+            del avg_corr_z
+            # and save
+            np.save(avg_corr_file%(sub, hemi), avg_corr)
+            #del avg_corr
+            
+        # threshold to maintain only top 10% of correlations
+        thr = np.percentile(avg_corr, 90)
+        
+        # add to cross subject average
+        print 'adding to avg '+sub
+        sub_avg[avg_corr>thr] += 1
+
+        #sub_avg += avg_corr
+        del avg_corr
+        
+        # increase sub count for division
+        sub_count += 1
+    
+    print 'dividing'
+    sub_avg = sub_avg / sub_count
+    print 'saving'
+    np.save(sub_avg_file%(hemi), sub_avg)
--- clustering_allsub.py
+++ clustering_allsub.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.stats as stats
 from sklearn.utils.arpack import eigsh
 from sklearn.cluster import KMeans
+import h5py
 
 
 '''
@@ -10,14 +11,20 @@ inputs
 '''
 hemis = ['rh']
 
-kclusts = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#
+#kclusts = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+kclusts = [5,7,17]
 embed = 3
-          
-#corr_file='/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_allsub_avg_corr.npy'
-corr_file='/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_allsub_avg_corr_thr.npy'
-mask_file='/scr/ilz3/myelinconnect/all_data_on_simple_surf/surfs/%s_mask.1D.roi'
-embed_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/thr/%s_embed_%s.csv"
-kmeans_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/thr/%s_kmeans_%s_embed_%s.csv"
+smooth='smooth_3'
+
+mask_file='/scr/ilz3/myelinconnect/all_data_on_simple_surf/masks/%s_mask.1D.roi'
+
+corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_%s_avg_corr.hdf5'
+embed_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/%s/thr/%s_embed_%s.csv"
+kmeans_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/%s/thr/%s_kmeans_%s_embed_%s.csv"
+#corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_%s_thr_per_session_corr.hdf5'
+#embed_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/%s/thr/%s_embed_%s.csv"
+#kmeans_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/%s/thr/%s_kmeans_%s_embed_%s.csv"
 
 
 '''
@@ -67,7 +74,10 @@ for hemi in hemis:
         n_components_kmeans=kclust
          
         print 'reading corr data'
-        corr = np.nan_to_num(np.load(corr_file%(hemi)))
+        f = h5py.File(corr_file%(hemi, smooth), 'r')
+        corr = np.zeros((f['shape']))
+        corr[np.triu_indices_from(corr, k=1)] = np.nan_to_num(np.asarray(f['upper']))
+        corr += corr.T
         all_vertex=range(corr.shape[0])
         
         print 'masking'
@@ -79,15 +89,17 @@ for hemi in hemis:
         
         
         try:
-            np.loadtxt(embed_file%(hemi, str(n_components_embedding)))
+            np.loadtxt(embed_file%(smooth, hemi, str(n_components_embedding)))
             
         except IOError: 
             
             print 'running embedding'
             K = (masked_corr + 1) / 2.  
+            del masked_corr
             v = np.sqrt(np.sum(K, axis=1)) 
             A = K/(v[:, None] * v[None, :])  
             del K
+            del v
             A = np.squeeze(A * [A > 0])
             embedding_results = runEmbed(A, n_components_embedding)
         
@@ -95,18 +107,15 @@ for hemi in hemis:
             for e in range(embedding_results.shape[1]):
                 embedding_recort[:,e]=recort(len(all_vertex), 
                                              embedding_results[:,e], cortex, 0)
-            np.savetxt(embed_file%(hemi, str(n_components_embedding)), 
+            np.savetxt(embed_file%(smooth, hemi, str(n_components_embedding)), 
                        embedding_recort, delimiter=",")
         
         print 'running kmeans'
         kmeans_results = runKmeans(embedding_results, n_components_kmeans)
         kmeans_recort = recort(len(all_vertex), kmeans_results, cortex, 1)
-        np.savetxt(kmeans_file%(hemi, str(n_components_kmeans), 
+        np.savetxt(kmeans_file%(smooth, hemi, str(n_components_kmeans), 
                                 str(n_components_embedding)), 
                    kmeans_recort, delimiter=",")
-        
-        #print 'subclustering'
-        #subclust_arr=subcluster(kmeans_recort, face['val'], n_vertices)
-        #np.savetxt(subclust_file, subclust_arr, delimiter=",")        
+      
         
         print 'done'
