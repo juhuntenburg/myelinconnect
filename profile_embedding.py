5af77a41d621d32f886f56d6a59247655f071d67
Julia Huntenburg <ju.huntenburg@gmail.com>
2016-02-17 21:49:29 +0100

    embeddings with new snr masks

--- profile_embedding.py
+++ profile_embedding.py
@@ -21,147 +21,145 @@ def chebapprox(profiles, degree):
     return cheb_coeffs, cheb_polynoms
 
 
-hemi='rh'
-masktype='025_5'
+hemis=['lh','rh']
+smooth= 'smooth_3'
 n_embedding = 10
-
-mesh_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/surfs/lowres_%s_d.vtk'%hemi
-mask_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/masks/%s_fullmask_%s.npy'
-t1_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/avg_%s_profiles.npy'%(hemi)
-#euclid_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/profile_embedding/%s_euclidian_dist_%s.hdf5'
-#corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/profile_embedding/%s_profile_corr_%s.hdf5'
-
-affinity_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/profile_embedding/%s_chebychev_affinity_%s_cauchy.hdf5'
-embed_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/profile_embedding/%s_mask_%s_%s_%s_cauchy.npy'
-embed_dict_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/profile_embedding/%s_mask_%s_%s_%s_cauchy_dict.pkl'
-
-v, f, d = read_vtk(mesh_file)
-t1=np.load(t1_file)
-full_shape=tuple((t1.shape[0], t1.shape[0]))
-
+affine_methods=['markov', 'cauchy']
 
 euclid = False
 corr = False
 affinity = True
 embed_corr = False
 embed_euclid = False
-embed_affinity = False
+embed_affinity = True
 
 
-if euclid:
-    print 'euclid'
-    t1_3_7_diff = sp.spatial.distance.pdist(t1[:,3:8], 'euclidean')
-    f = h5py.File(euclid_file%('rh', '3_7'), 'w')
-    f.create_dataset('upper', data=t1_3_7_diff)
-    f.create_dataset('shape', data=full_shape)
-    f.close()
-    del t1_3_7_diff
-    
-#     t1_2_8_diff = sp.spatial.distance.pdist(t1[:,2:9], 'euclidean')
-#     f = h5py.File(euclid_file%('rh', '2_8'), 'w')
-#     f.create_dataset('upper', data=t1_2_8_diff)
-#     f.create_dataset('shape', data=full_shape)
-#     f.close()
-#     del t1_2_8_diff
-    
-    
-if corr:
-    print 'corr'
-    t1_3_7_corr = hcp_corr.corrcoef_upper(t1[:,3:8])
-    f = h5py.File(corr_file%('rh', '3_7'), 'w')
-    f.create_dataset('upper', data=t1_3_7_corr)
-    f.create_dataset('shape', data=full_shape)
-    f.close()
-    #del t1_3_7_corr
-    
-if affinity:
-    print 'chebychev'
-    t1_3_7 = t1[:,3:8]
-    coeff, poly = chebapprox(t1_3_7, degree=4)
-    print 'affinity'
-    t1_3_7_affine = dist.compute_affinity(coeff, method='cauchy')
-    t1_3_7_affine = t1_3_7_affine[np.triu_indices_from(t1_3_7_affine, k=1)]
-    f = h5py.File(affinity_file%('rh', '3_7'), 'w')
-    f.create_dataset('upper', data=t1_3_7_affine)
-    f.create_dataset('shape', data=full_shape)
-    f.close()
-    del t1_3_7_
 
-
-if embed_corr:
+for hemi in hemis: 
     
-    print 'embedding'
-    
-    if corr:
-        upper_corr = t1_3_7_corr
-    else:
-        # load upper triangular avg correlation matrix
-        f = h5py.File(corr_file%('rh', '3_7'), 'r')
-        upper_corr = np.asarray(f['upper'])
-        full_shape = tuple(f['shape'])
-        f.close()
+    for affine_method in affine_methods:
         
+        print hemi, affine_method
 
-    mask = np.load(mask_file%(hemi, masktype))
-    embedding_recort, embedding_dict = embedding(upper_corr, full_shape, mask, n_embedding)
-
-    np.save(embed_file%(hemi, masktype, 'corr_embed', str(n_embedding)),embedding_recort)
-    pkl_out = open(embed_dict_file%(hemi, masktype, 'corr_embed', str(n_embedding)), 'wb')
-    pickle.dump(embedding_dict, pkl_out)
-    pkl_out.close()
-    
-
-
-if embed_euclid:
-    
-    print 'loading'
-    
-    if not euclid:
-        # load upper triangular avg correlation matrix
-        f = h5py.File(euclid_file%('rh', '3_7'), 'r')
-        upper_diff = np.asarray(f['upper'])
-        full_shape = tuple(f['shape'])
-        f.close()
-    else:
-        upper_diff = t1_3_7_diff
+        mesh_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/surfs/lowres_%s_d.vtk'%hemi
+        mask_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/masks/%s_fullmask.npy'%hemi
+        #t1_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/%s/avg_%s_profiles_%s.npy'%(smooth, hemi, smooth)
+        t1_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/%s/avg_%s_coeffs_%s.npy'%(smooth, hemi, smooth)
+        #euclid_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/profile_embedding/%s_euclidian_dist_%s.hdf5'
+        #corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/t1/profile_embedding/%s_profile_corr_%s.hdf5'
+        affinity_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/embed/profiles/%s_%s_cheb_affinity_%s.hdf5'%(hemi, smooth, affine_method)
+        embed_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/embed/profiles/%s_%s_embedding_%s_%s.npy'%(hemi, smooth, str(n_embedding), affine_method)
+        embed_dict_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/embed/profiles/%s_%s_embedding_dict_%s_%s.pkl'%(hemi, smooth, str(n_embedding), affine_method)
         
-    full_diff = np.zeros(tuple(full_shape))
-    full_diff[np.triu_indices_from(full_diff, k=1)] = np.nan_to_num(upper_diff)
-    full_diff += full_diff.T
-    
-    print 'normalizing'
-    full_diff_norm = dist.compute_affinity(full_diff)
-    #del full_diff
-    upper_diff_norm = full_diff_norm[np.triu_indices_from(full_diff_norm, k=1)]
-    #del full_diff_norm
-    
-    print 'embedding'
-    mask = np.load(mask_file%(hemi, masktype))
-    embedding_recort, embedding_dict = embedding(upper_diff_norm, full_shape, mask, n_embedding)
+        v, f, d = read_vtk(mesh_file)
+        t1=np.load(t1_file)
+        full_shape=tuple((t1.shape[0], t1.shape[0]))
 
-    np.save(embed_file%(hemi, masktype, 'euclid_embed', str(n_embedding)),embedding_recort)
-    pkl_out = open(embed_dict_file%(hemi, masktype, 'euclid_embed', str(n_embedding)), 'wb')
-    pickle.dump(embedding_dict, pkl_out)
-    pkl_out.close()
-    
 
-if embed_affinity:
-    
-    if not affinity:
-        f = h5py.File(affinity_file%('rh', '3_7'), 'r')
-        upper_affine = np.asarray(f['upper'])
-        full_shape = tuple(f['shape'])
-        f.close()
-    else:
-        upper_affine = t1_3_7_affine.copy()
-        del t1_3_7_affine
+        if euclid:
+            print 'euclid'
+            t1_3_7_diff = sp.spatial.distance.pdist(t1[:,3:8], 'euclidean')
+            f = h5py.File(euclid_file%('rh', '3_7'), 'w')
+            f.create_dataset('upper', data=t1_3_7_diff)
+            f.create_dataset('shape', data=full_shape)
+            f.close()
+            del t1_3_7_diff
+            
+        if corr:
+            print 'corr'
+            t1_3_7_corr = hcp_corr.corrcoef_upper(t1[:,3:8])
+            f = h5py.File(corr_file%('rh', '3_7'), 'w')
+            f.create_dataset('upper', data=t1_3_7_corr)
+            f.create_dataset('shape', data=full_shape)
+            f.close()
+            
+        if affinity:
+            #print 'chebychev'
+            #t1_3_7 = t1[:,3:8]
+            #coeff, poly = chebapprox(t1_3_7, degree=4)
+            coeff = t1.copy()
+            print 'affinity'
+            t1_3_7_affine = dist.compute_affinity(coeff, method=affine_method)
+            t1_3_7_affine = t1_3_7_affine[np.triu_indices_from(t1_3_7_affine, k=1)]
+            f = h5py.File(affinity_file%(hemi, smooth), 'w')
+            f.create_dataset('upper', data=t1_3_7_affine)
+            f.create_dataset('shape', data=full_shape)
+            f.close()
         
-    
-    mask = np.load(mask_file%(hemi, masktype))
-    embedding_recort, embedding_dict = embedding(upper_affine, full_shape, mask, n_embedding)
-
-    np.save(embed_file%(hemi, masktype, 'affine_embed', str(n_embedding)),embedding_recort)
-    pkl_out = open(embed_dict_file%(hemi, masktype, 'affine_embed', str(n_embedding)), 'wb')
-    pickle.dump(embedding_dict, pkl_out)
-    pkl_out.close()
-
+        if embed_corr:
+            
+            print 'embedding'
+            
+            if corr:
+                upper_corr = t1_3_7_corr
+            else:
+                # load upper triangular avg correlation matrix
+                f = h5py.File(corr_file%('rh', '3_7'), 'r')
+                upper_corr = np.asarray(f['upper'])
+                full_shape = tuple(f['shape'])
+                f.close()
+                
+        
+            mask = np.load(mask_file%(hemi, masktype))
+            embedding_recort, embedding_dict = embedding(upper_corr, full_shape, mask, n_embedding)
+        
+            np.save(embed_file%(hemi, masktype, 'corr_embed', str(n_embedding)),embedding_recort)
+            pkl_out = open(embed_dict_file%(hemi, masktype, 'corr_embed', str(n_embedding)), 'wb')
+            pickle.dump(embedding_dict, pkl_out)
+            pkl_out.close()
+            
+        
+        
+        if embed_euclid:
+            
+            print 'loading'
+            
+            if not euclid:
+                # load upper triangular avg correlation matrix
+                f = h5py.File(euclid_file%('rh', '3_7'), 'r')
+                upper_diff = np.asarray(f['upper'])
+                full_shape = tuple(f['shape'])
+                f.close()
+            else:
+                upper_diff = t1_3_7_diff
+                
+            full_diff = np.zeros(tuple(full_shape))
+            full_diff[np.triu_indices_from(full_diff, k=1)] = np.nan_to_num(upper_diff)
+            full_diff += full_diff.T
+            
+            print 'normalizing'
+            full_diff_norm = dist.compute_affinity(full_diff)
+            #del full_diff
+            upper_diff_norm = full_diff_norm[np.triu_indices_from(full_diff_norm, k=1)]
+            #del full_diff_norm
+            
+            print 'embedding'
+            mask = np.load(mask_file%(hemi, masktype))
+            embedding_recort, embedding_dict = embedding(upper_diff_norm, full_shape, mask, n_embedding)
+        
+            np.save(embed_file%(hemi, masktype, 'euclid_embed', str(n_embedding)),embedding_recort)
+            pkl_out = open(embed_dict_file%(hemi, masktype, 'euclid_embed', str(n_embedding)), 'wb')
+            pickle.dump(embedding_dict, pkl_out)
+            pkl_out.close()
+            
+        
+        if embed_affinity:
+            
+            if not affinity:
+                f = h5py.File(affinity_file%('rh', '3_7'), 'r')
+                upper_affine = np.asarray(f['upper'])
+                full_shape = tuple(f['shape'])
+                f.close()
+            else:
+                upper_affine = t1_3_7_affine.copy()
+                del t1_3_7_affine
+                
+            
+            mask = np.load(mask_file%(hemi))
+            embedding_recort, embedding_dict = embedding(upper_affine, full_shape, mask, n_embedding)
+        
+            np.save(embed_file ,embedding_recort)
+            pkl_out = open(embed_dict_file, 'wb')
+            pickle.dump(embedding_dict, pkl_out)
+            pkl_out.close()
+        
--- run.py
+++ run.py
@@ -18,31 +18,30 @@ subjects.remove('KSMT')
 smooths=['smooth_3'] #, 'raw', 'smooth_2']
 hemis = ['rh', 'lh']#, 'lh']
 sessions = ['1_1', '1_2' , '2_1', '2_2']
-masktype = '025_5'
+#masktype = '025_5'
 n_embedding = 10
-n_kmeans = range(2,21)
+#n_kmeans = range(2,21)
 #n_kmeans = [5,10,15]
 
-calc_corr = True
-calc_embed = False
+calc_corr = False
+calc_embed = True
 calc_cluster = False
 calc_subcluster = False
 
 rest_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/rest/%s/%s_%s_rest%s_%s.npy'
 #thr_corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_%s_thr%s_per_session_corr.hdf5'
 corr_file = '/scr/ilz3/myelinconnect/all_data_on_simple_surf/corr/%s_%s_avg_corr.hdf5'
-embed_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/embed/%s_%s_mask_%s_embed_%s.npy"
-embed_dict_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/embed/%s_%s_mask_%s_embed_%s_dict.pkl"
-#kmeans_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/%s/mask_%s/%s_embed_%s_kmeans_%s.npy"
+embed_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/embed/%s_%s_embed_%s.npy"
+embed_dict_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/embed/%s_%s_embed_%s_dict.pkl"
+#kmeans_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/%s/%s_embed_%s_kmeans_%s.npy"
 #mask_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/masks/%s_fullmask_%s.npy"
+mask_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/masks/%s_fullmask.npy"
 
 mesh_file="//scr/ilz3/myelinconnect/all_data_on_simple_surf/surfs/lowres_%s_d.vtk"
 #subclust_file="/scr/ilz3/myelinconnect/all_data_on_simple_surf/clust/%s/mask_%s/%s_embed_%s_kmeans_%s_subclust.npy"
 
 for smooth in smooths:
     print 'smooth '+smooth
-    
-    print 'mask '+masktype
 
     for hemi in hemis:
         print 'hemi '+hemi
@@ -78,11 +77,11 @@ for smooth in smooths:
                 full_shape = tuple(f['shape'])
                 f.close()
 
-            mask = np.load(mask_file%(hemi, masktype))
+            mask = np.load(mask_file%(hemi))
             embedding_recort, embedding_dict = embedding(upper_corr, full_shape, mask, n_embedding)
 
-            np.save(embed_file%(hemi, smooth, masktype, str(n_embedding)),embedding_recort)
-            pkl_out = open(embed_dict_file%(hemi, smooth, masktype, str(n_embedding)), 'wb')
+            np.save(embed_file%(hemi, smooth, str(n_embedding)),embedding_recort)
+            pkl_out = open(embed_dict_file%(hemi, smooth, str(n_embedding)), 'wb')
             pickle.dump(embedding_dict, pkl_out)
             pkl_out.close()
             
@@ -91,11 +90,11 @@ for smooth in smooths:
         if calc_cluster:
             if not calc_embed:
                 embedding_recort = np.load(embed_file%(smooth, masktype, hemi, str(n_embedding)))
-                mask = np.load(mask_file%(hemi, masktype))
+                mask = np.load(mask_file%(hemi))
             for nk in n_kmeans:
                 print 'clustering %s'%str(nk)
                 kmeans_recort = kmeans(embedding_recort, nk, mask)
-                np.save(kmeans_file%(smooth, masktype, hemi, str(n_embedding), str(nk)),
+                np.save(kmeans_file%(smooth, hemi, str(n_embedding), str(nk)),
                         kmeans_recort)
                         
 
