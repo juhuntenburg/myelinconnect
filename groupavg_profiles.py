56c15f62f1601edd6c7b0bf100ee2f74d3c5dcba
Julia Huntenburg <ju.huntenburg@gmail.com>
2016-07-11 18:33:59 +0200

    prepare comparing compartments to fc1

--- groupavg_profiles.py
+++ groupavg_profiles.py
@@ -2,23 +2,31 @@ from vtk_rw import read_vtk, write_vtk
 import numpy as np
 
 
-smooth = 'smooth_6'
-hemis = ['rh', 'lh']
+compartment = 'lower'
+hemis = ['lh', 'rh']
 
 vtk_file = '/scr/ilz3/myelinconnect/new_groupavg/profiles/%s/%s/%s_lowres_new_avgsurf_groupdata.vtk'
 avg_npy_file = '/scr/ilz3/myelinconnect/new_groupavg/profiles/%s/%s_group_avg_profiles_%s.npy'
-avg_vtk_file = '/scr/ilz3/myelinconnect/new_groupavg/profiles/%s/%s_group_avg_profiles_%s.vtk'
+#avg_vtk_file = '/scr/ilz3/myelinconnect/new_groupavg/profiles/%s/%s_group_avg_profiles_%s.vtk'
+both_hemi_npy_file = '/scr/ilz3/myelinconnect/new_groupavg/profiles/%s/both_group_avg_profiles_%s.npy'
 
+
+both_hemis = []
 for hemi in hemis:
     
     avg_list = []
+    group_list = []
     
     for pro in range(11):
         
-        v, f, d = read_vtk(vtk_file%(smooth, str(pro), hemi))
+        v, f, d = read_vtk(vtk_file%(compartment, str(pro), hemi))
         avg_list.append(np.mean(d, axis=1))
         
-        
     avg_array = np.asarray(avg_list).T
-    np.save(avg_npy_file%(smooth, hemi, smooth), avg_array)
-    write_vtk(avg_vtk_file%(smooth, hemi,smooth), v,f, data=avg_array)
+    both_hemis.append(avg_array)
+    
+    np.save(avg_npy_file%(compartment, hemi, compartment), avg_array)
+    #write_vtk(avg_vtk_file%(compartment, hemi, compartment), v,f, data=avg_array)
+
+both = np.concatenate((both_hemis[0], both_hemis[1]))
+np.save(both_hemi_npy_file%compartment, both)
