8aa206a5eb57b541b125405fd9e178d937902bd6
Julia Huntenburg <ju.huntenburg@gmail.com>
2016-07-11 18:03:27 +0200

    finish script for raw comparison

--- fc_vs_t1_raw.py
+++ fc_vs_t1_raw.py
@@ -2,16 +2,53 @@ from __future__ import division
 import numpy as np
 import h5py
 from scipy.spatial.distance import pdist
+import scipy.stats as stats
 
 fc_file = '/scr/ilz3/myelinconnect/new_groupavg/corr/both_smooth_3_avg_corr.hdf5'
 t1_file = corr_file = '/scr/ilz3/myelinconnect/new_groupavg/corr/both_smooth_avg_t1_dist.hdf5'
+mask_file="/scr/ilz3/myelinconnect/new_groupavg/masks/fullmask_lh_rh_new.npy"
+upper_mask_file="/scr/ilz3/myelinconnect/new_groupavg/masks/upper_fullmask_lh_rh_new.npy"
 
-# load t1 data
-print 'load data'
-t1_left = np.load(t1_file%('lh'))
-t1_right = np.load(t1_file%('rh'))
-t1 = np.concatenate((t1_left, t1_right))
-size = t1[:,0].shape[0]
+
+print 'create mask'
+f = h5py.File(fc_file, 'r')
+full_shape = tuple(f['shape'])
+f.close()
+
+mask = np.load(mask_file)
+
+fake_mat = np.zeros(full_shape)
+fake_mat[mask] = 1
+fake_mat[:,mask] = 1
+
+upper_fake = fake_mat[np.triu_indices_from(fake_mat)]
+del fake_mat
+upper_mask=np.where(upper_fake)[0]
+del upper_fake
+np.save(upper_mask_file, upper_mask)
+
+
+
+print 'load fc'
+f = h5py.File(fc_file, 'r')
+fc = np.asarray(f['upper'])
+f.close()
+
+print 'mask fc'
+fc = np.delete(fc, upper_mask)
+
+print 'load t1'
+f = h5py.File(t1_file, 'r')
+t1 = np.asarray(f['upper'])
+f.close()
+
+print 'mask t1'
+t1 = np.delete(t1, upper_mask)
+
+print 'uncorrected df', fc.shape[0]
+print 'Pearson r', stats.pearsonr(fc, t1)
+
+
 
 
 # calculate t1 distances for each subject and average
